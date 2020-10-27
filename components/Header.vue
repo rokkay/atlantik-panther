15fba998a943483723aedc17378fab1dac6c613d
Rokkay <rayaleonjosecarlos@gmail.com>
2020-10-27 19:47:45 +0100

    Initial commit

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav class="flex">
+      <div class="">
+        <a href=""> <img src="" alt="logo"> </a>
+      </div>
+      <div class="">
+        <a href="">Link1</a><a href="">Link2</a><a href="">Link3</a><a href="">Link4</a><a href="">Link5</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/_lang/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        {{ $t('home.title') }}
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+        <NuxtLink v-if="$i18n.locale === 'es'" :to="`/pt` + $route.fullPath" class="Header__Link" active-class="none" exact>
+          {{ $t('links.portuguese') }}
+        </NuxtLink>
+        <NuxtLink v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" class="Header__Link" active-class="none" exact>
+          {{ $t('links.spanish') }}
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+
+</style>
